99a6b89dc48d4980a61bb940b725dcf0c75657f8
Ubuntu <ubuntu@ip-172-31-60-95.ec2.internal>
2017-02-16 03:23:53 +0000

    trainperdev

--- bin/run_experiment.sh
+++ bin/run_experiment.sh
@@ -3,4 +3,4 @@ DIVIDER=1
 TRSIZE=$(expr 45000 / $DIVIDER)
 VASIZE=$(expr 5000 / $DIVIDER)
 WARMSTART=0
-nohup th adaptive_main.lua -dataRoot cifar.torch/ -resultFolder results/ -deathRate 0.5 -trsize $TRSIZE -vasize $VASIZE -alphaLR 0.5 -warmStartEpochs $WARMSTART &
+nohup th adaptive_main.lua -trainPerDev 5 -dataRoot cifar.torch/ -resultFolder results/ -deathRate 0.5 -trsize $TRSIZE -vasize $VASIZE -alphaLR 0.5 -warmStartEpochs $WARMSTART &
